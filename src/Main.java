d6c4f8b665a08791492ff4134bc1b277f469dfce
david <davidsanchez363@gmail.com>
2024-05-29 16:43:25 -0500

    [Mensaje adecuado]

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int[] object= {1, 2, 3 ,4 ,69,100};
+        
     }
 }
